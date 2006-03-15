dc48b3480fdb03a882dc88ab00f37826e220a494
mud <devnull@localhost>
2006-03-15 08:51:34 +0000

    fix

--- src/fight.cpp
+++ src/fight.cpp
@@ -2720,7 +2720,7 @@ inline int do_punctual(CHAR_DATA *ch, CHAR_DATA *victim, OBJ_DATA *wielded)
 
 	if (wielded)
 		wapp = (int)(GET_OBJ_SKILL(wielded) == SKILL_BOWS) ?
-					GET_OBJ_WEIGHT(wielded) * 2 / 3 : GET_OBJ_WEIGHT(wielded);
+					GET_OBJ_WEIGHT(wielded) / 2 : GET_OBJ_WEIGHT(wielded);
 	else
 		wapp = 0;
 
