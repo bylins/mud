2116d6ae9b21d0466eed171490d88099f9b0168d
krodo <devnull@localhost>
2006-09-23 20:06:54 +0000

    vata

--- src/handler.h
+++ src/handler.h
@@ -63,8 +63,6 @@ int get_number(char **name);
 
 /* ******** objects *********** */
 bool equal_obj(OBJ_DATA *obj_one, OBJ_DATA *obj_two);
-void move_obj_to_top(OBJ_DATA *obj, OBJ_DATA **list_start);
-
 void obj_to_char(OBJ_DATA * object, CHAR_DATA * ch);
 void obj_from_char(OBJ_DATA * object);
 
