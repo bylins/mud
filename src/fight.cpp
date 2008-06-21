ad413a23fa9967d1179c23f0e41247c7ba406ad8
krodo <devnull@localhost>
2008-06-21 17:01:00 +0000

    *** empty log message ***

--- src/fight.cpp
+++ src/fight.cpp
@@ -2629,20 +2629,21 @@ int damage(CHAR_DATA * ch, CHAR_DATA * victim, int dam, int attacktype, int mayf
 		{
 			sprintf(buf2, "%s killed by %s at %s", GET_NAME(victim),
 				GET_NAME(ch), IN_ROOM(victim) != NOWHERE ? world[IN_ROOM(victim)]->name : "NOWHERE");
+			log(buf2);
 
 			if ((!IS_NPC(ch) || (ch->master && !IS_NPC(ch->master)))
 				&& (RENTABLE(victim) && !ROOM_FLAGGED(IN_ROOM(victim), ROOM_ARENA)))
 			{
 				mudlog(buf2, BRF, LVL_IMPL, SYSLOG, 0);
+				if (IS_NPC(ch)
+					&& (AFF_FLAGGED(ch, AFF_CHARM) || IS_HORSE(ch))
+					&& ch->master && !IS_NPC(ch->master))
+				{
+					sprintf(buf2, "%s подчиняется %s.", GET_NAME(ch), GET_PAD(ch->master, 2));
+					mudlog(buf2, BRF, LVL_IMPL, SYSLOG, TRUE);
+				}
 			}
-			log(buf2);
 
-			if (IS_NPC(ch) &&
-			    (AFF_FLAGGED(ch, AFF_CHARM) || IS_HORSE(ch)) && ch->master && !IS_NPC(ch->master))
-			{
-				sprintf(buf2, "%s подчиняется %s.", GET_NAME(ch), GET_PAD(ch->master, 2));
-				mudlog(buf2, BRF, LVL_IMPL, SYSLOG, TRUE);
-			}
 			if (MOB_FLAGGED(ch, MOB_MEMORY))
 				forget(ch, victim);
 		}
