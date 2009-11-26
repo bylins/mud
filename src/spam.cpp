526e2e0475556e8dbd66a6b0a3257882b55ecb5a
krodo <devnull@localhost>
2009-11-26 16:09:04 +0000

    *** empty log message ***

--- src/spam.cpp
+++ src/spam.cpp
@@ -3,8 +3,8 @@
 // Part of Bylins http://www.mud.ru
 
 #include <list>
+#include <sstream>
 #include "spam.hpp"
-#include "db.h"
 #include "utils.h"
 #include "char.hpp"
 #include "comm.h"
@@ -21,13 +21,21 @@ const int MAX_MESSAGE_RUNNING = 10;
 const unsigned MESSAGE_LIST_SIZE = 31;
 // макс сообщений от одного чара за последние MESSAGE_LIST_SIZE сообщений
 const int MAX_MESSAGE_TOTAL = 15;
+// кол-во учитыаемых в MAX_MESSAGE_TOTAL секунд между первой и текущей мессагой
+// т.е. если даже чар сказал MAX_MESSAGE_TOTAL мессаг из последних MESSAGE_LIST_SIZE,
+// но при этом между ними есть приличный временной зазор
+const int MAX_TIME_TOTAL = 45;
 // секунд между мессагами от одного чара (вместо тупо лага)
-const int MIN_MESSAGE_TIME = 2;
+const int MIN_MESSAGE_TIME = 1;
+
+enum { NORMAL_FLAG, RUNNING_FLAG, MIN_TIME_FLAG, TOTAL_FLAG };
 
 typedef std::list<std::pair<long, time_t> > UidListType;
-
 UidListType offtop_list;
 
+/**
+*
+*/
 void add_to_list(UidListType &list, long uid)
 {
 	list.push_front(std::make_pair(uid, time(0)));
@@ -37,9 +45,13 @@ void add_to_list(UidListType &list, long uid)
 	}
 };
 
-bool check_list(const UidListType &list, long uid)
+/**
+*
+*/
+int check_list(const UidListType &list, long uid)
 {
 	int total = 0, running = 0;
+	time_t first_message = time(0);
 	for (UidListType::const_iterator i = offtop_list.begin(); i != offtop_list.end(); ++i)
 	{
 		if (uid != i->first)
@@ -53,46 +65,49 @@ bool check_list(const UidListType &list, long uid)
 				++running;
 				if (running >= MAX_MESSAGE_RUNNING)
 				{
-					return false;
+					return RUNNING_FLAG;
 				}
 			}
 			if ((time(0) - i->second) < MIN_MESSAGE_TIME)
 			{
-					return false;
+					return MIN_TIME_FLAG;
 			}
 			++total;
+			first_message = i->second;
 		}
 	}
-	if (total >= MAX_MESSAGE_TOTAL)
+	if (total >= MAX_MESSAGE_TOTAL
+		&& (time(0) - first_message) < MAX_TIME_TOTAL)
 	{
-		return false;
+		return TOTAL_FLAG;
 	}
-	return true;
+	return NORMAL_FLAG;
 };
 
 /**
-* \return - true = чар может говорить
+*
 */
-bool add_message(int mode, long uid)
+int add_message(int mode, long uid)
 {
 	switch(mode)
 	{
 	case OFFTOP_MODE:
-		if (check_list(offtop_list, uid))
+		int flag = check_list(offtop_list, uid);
+		if (NORMAL_FLAG == flag)
 		{
 			add_to_list(offtop_list, uid);
 		}
 		else
 		{
-			return false;
+			return flag;
 		}
 		break;
 	default:
-		sprintf(buf, "SYSERROR: мы не должны были сюда попасть (%s %s %d)", __FILE__, __func__, __LINE__);
-		return false;
+		log("SYSERROR: мы не должны были сюда попасть (%s %s %d)", __FILE__, __func__, __LINE__);
+		return NORMAL_FLAG;
 	};
 
-	return true;
+	return NORMAL_FLAG;
 }
 
 } // namespace
@@ -100,16 +115,41 @@ bool add_message(int mode, long uid)
 namespace SpamSystem
 {
 
+/**
+*
+*/
 bool check(CHAR_DATA *ch, int mode)
 {
-	if (!add_message(mode, GET_UNIQUE(ch)))
+	int flag = add_message(mode, GET_UNIQUE(ch));
+	if (NORMAL_FLAG != flag)
 	{
-		send_to_char(ch, "Сообщение заблокировано спам-контролем.\r\n");
+		std::stringstream text;
+		text << "Спам-контроль: ";
+		switch (flag)
+		{
+		case RUNNING_FLAG:
+			text << "больше " << MAX_MESSAGE_RUNNING << " сообщений подряд";
+			break;
+		case MIN_TIME_FLAG:
+			text << "интервал между сообщениями меньше " << MIN_MESSAGE_TIME << " секунды";
+			break;
+		case TOTAL_FLAG:
+			text << "высокий процент Ваших сообщений за последние " << MAX_TIME_TOTAL << " секунд";
+			break;
+		default:
+			log("SYSERROR: мы не должны были сюда попасть (%s %s %d)", __FILE__, __func__, __LINE__);
+			return true;
+		};
+		text << ".\r\n";
+		send_to_char(text.str(), ch);
 		return false;
 	}
 	return true;
 }
 
+/**
+*
+*/
 void check_new_channels(CHAR_DATA *ch)
 {
 	if (MIN_OFFTOP_LVL == GET_LEVEL(ch) && !GET_REMORT(ch))
