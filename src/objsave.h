657109be4e0f3dde37ea08e2c968db8c15461e29
krodo <devnull@localhost>
2009-12-18 14:03:43 +0000

    *** empty log message ***

---
+++ src/objsave.h
@@ -0,0 +1,10 @@
+// $RCSfile$     $Date$     $Revision$
+// Copyright (c) 2009 Krodo
+// Part of Bylins http://www.mud.ru
+
+#ifndef OBJSAVE_HPP_INCLUDED
+#define OBJSAVE_HPP_INCLUDED
+
+void write_one_object(std::stringstream &out, OBJ_DATA * object, int location);
+
+#endif // OBJSAVE_HPP_INCLUDED
