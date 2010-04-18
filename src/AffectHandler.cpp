53ecee23b018f26d90846f877906f76fdde51f6d
sventovit <devnull@localhost>
2010-04-18 11:35:11 +0000

    affect handler

---
+++ src/AffectHandler.cpp
@@ -0,0 +1,52 @@
+#include "AffectHandler.hpp"
+#include <iostream>
+#include "utils.h"
+#include "handler.h"
+#include "spells.h"
+
+//нужный Handler вызывается в зависимости от типа передаваемых параметров
+void LackyAffectHandler::Handle( DamageActorParameters& params )
+{
+	if (params.damage>0) damFromMe_ = true;
+	params.damage += params.damage*(round_*4)/100;
+}
+
+void LackyAffectHandler::Handle( DamageVictimParameters& params )
+{
+	if (params.damage>0) damToMe_ = true;
+}
+
+AFFECT_DATA*  find_affect(CHAR_DATA* ch, int afftype)
+{
+	AFFECT_DATA* aff;
+	for(aff=ch->affected;aff;aff=aff->next)
+	{
+		if (aff->type == afftype)
+			return aff;
+	}
+	return 0;
+}
+
+void LackyAffectHandler::Handle( BattleRoundParameters& params )
+{
+	AFFECT_DATA* af = find_affect(params.ch, SPELL_LACKY);
+	if (damFromMe_&&!damToMe_){
+		if (round_<5) {
+			++round_;
+		}
+	} 
+	else {
+		round_= 0;
+	}
+	if (af) af->modifier=round_*2;
+	damToMe_=false;
+	damFromMe_=false;
+}
+
+void LackyAffectHandler::Handle( StopFightParameters& params )
+{
+	AFFECT_DATA* af = find_affect(params.ch, SPELL_LACKY);
+	if (af) af->modifier = 0;
+	round_=0;
+	damFromMe_=damToMe_=false;
+}
---
+++ src/AffectHandler.hpp
@@ -0,0 +1,60 @@
+#ifndef _AFFECTHANDLER_HPP_
+#define _AFFECTHANDLER_HPP_
+#include "char.hpp"
+
+//классы для конфигурирования обработчиков Handler()
+class DamageActorParameters {
+public:
+	DamageActorParameters(CHAR_DATA* act, CHAR_DATA* vict, int dam) : ch(act), opponent(vict), damage(dam) {};
+	CHAR_DATA* ch;
+	CHAR_DATA* opponent;
+	int damage;
+};
+
+class DamageVictimParameters {
+public:
+	DamageVictimParameters(CHAR_DATA* act, CHAR_DATA* vict, int dam) : ch(vict), opponent(act), damage(dam) {};
+	CHAR_DATA* ch;
+	CHAR_DATA* opponent;
+	int damage;
+};
+
+
+class BattleRoundParameters{
+public:
+	BattleRoundParameters(CHAR_DATA* actor) : ch(actor) {};
+	CHAR_DATA* ch;
+};
+
+class StopFightParameters{
+public:
+	StopFightParameters(CHAR_DATA* actor) : ch(actor) {};
+	CHAR_DATA* ch;
+};
+//инфтерфейс для обработчиков аффектов
+class IAffectHandler
+{
+public:
+	IAffectHandler(void) {};
+	~IAffectHandler(void) {};
+	virtual void Handle(DamageActorParameters& params) {};
+	virtual void Handle(DamageVictimParameters& params) {};
+	virtual void Handle(BattleRoundParameters& params) {};
+	virtual void Handle(StopFightParameters& params) {};
+};
+
+class LackyAffectHandler : public IAffectHandler{
+public:
+	LackyAffectHandler() : round_(0), damToMe_(false), damFromMe_(false) {};
+	~LackyAffectHandler() {};
+	virtual void Handle(DamageActorParameters& params);
+	virtual void Handle(BattleRoundParameters& params);
+	virtual void Handle(DamageVictimParameters& params);
+	virtual void Handle(StopFightParameters& params);
+private:
+	int round_;
+	bool damToMe_;
+	bool damFromMe_;
+};
+
+#endif
