d749b8e06b0665287d7fd5b08c82191e46ab6eac
polud <devnull@localhost>
2011-10-04 16:09:58 +0000

    animal morphs test

---
+++ src/morph.cpp
@@ -0,0 +1,336 @@
+#include "pugixml.hpp"
+#include "utils.h"
+#include "screen.h"
+#include "structs.h"
+#include "interpreter.h"
+#include "handler.h"
+#include "skills.h"
+#include "spells.h"
+#include "char.hpp"
+#include "comm.h"
+#include "db.h"
+#include "morph.hpp"
+
+MorphListType MorphList;
+
+short MIN_WIS_FOR_MORPH=0;
+
+string AnimalMorph::GetMorphDesc()
+{
+	string desc = "Неведома зверушка";
+	for (DescListType::const_iterator it = descList_.begin();it != descList_.end(); ++it)
+		if (it->fromLevel <= ch_->get_level()+ch_->get_remort())
+			desc = it->desc;
+		else break;
+
+	return desc;
+}
+
+string NormalMorph::GetMorphDesc()
+{
+	return ch_->get_name();
+}
+
+string NormalMorph::GetMorphTitle()
+{
+	return ch_->race_or_title();
+};
+
+string AnimalMorph::GetMorphTitle()
+{
+	return string(ch_->get_name()) + " - " + GetMorphDesc();
+};
+
+int NormalMorph::get_trained_skill(int skill_num)
+{
+	return ch_->get_inborn_skill(skill_num);
+}
+
+int AnimalMorph::get_trained_skill(int skill_num)
+{
+	CharSkillsType::iterator it = skills_.find(skill_num);
+	if (it != skills_.end())
+	{
+		return it->second;
+	}
+	return 0;
+}
+
+void NormalMorph::set_skill(int skill_num, int percent)
+{
+	ch_->set_skill(skill_num, percent);
+}
+
+void AnimalMorph::set_skill(int skill_num, int percent)
+{
+	CharSkillsType::iterator it = skills_.find(skill_num);
+	if (it != skills_.end())
+	{
+		int diff = percent - it->second;//Polud	пока все снижения уровня скилов в звериной форме уходят в /dev/null
+		if (diff > 0 && number(1,2)==2)//Polud в звериной форме вся прокачка идет в оборотничество
+		{
+			sprintf(buf, "%sВаши успехи сделали Вас опытнее в оборотничестве.%s\r\n", CCICYN(ch_, C_NRM), CCINRM(ch_, C_NRM));
+			send_to_char(buf, ch_); 		
+			skills_[SKILL_MORPH]+= diff;
+		}
+	}
+}
+
+int NormalMorph::GetStr() const {return ch_->get_inborn_str();}
+void NormalMorph::SetStr(int str) {ch_->set_str(str);}
+int NormalMorph::GetIntel() const  {return ch_->get_inborn_int();}
+void NormalMorph::SetIntel(int intel) {ch_->set_int(intel);}
+int NormalMorph::GetWis()const  {return ch_->get_inborn_wis();}
+void NormalMorph::SetWis(int wis) {ch_->set_wis(wis);}
+int NormalMorph::GetDex()const  {return ch_->get_inborn_dex();}
+void NormalMorph::SetDex(int dex) {ch_->set_dex(dex);}
+int NormalMorph::GetCha() const {return ch_->get_inborn_cha();}
+void NormalMorph::SetCha(int cha) {ch_->set_cha(cha);}
+int NormalMorph::GetCon() const {return ch_->get_inborn_con();}
+void NormalMorph::SetCon(int con) {ch_->set_con(con);}
+
+
+void ShowKnownMorphs(CHAR_DATA *ch)
+{
+	if (ch->is_morphed())
+	{
+		send_to_char("Чтобы вернуть себе человеческий облик - нужно 'обернуться назад'\r\n", ch);
+		return;
+	}
+	std::list<string> knownMorphs = ch->get_morphs();
+	if (knownMorphs.empty())
+		send_to_char("На сей момент никакие формы Вам неизвестны...\r\n", ch);
+	else
+		send_to_char("Вы можете обернуться:\r\n", ch);
+
+	for(std::list<string>::const_iterator it = knownMorphs.begin(); it!= knownMorphs.end(); ++it)
+		send_to_char("   "+MorphList[*it]->PadName()+"\r\n", ch);
+};
+
+string FindMorphId(CHAR_DATA * ch, char *arg)
+{
+	std::list<string> morphsList = ch->get_morphs();
+	for(std::list<string>::const_iterator it = morphsList.begin(); it != morphsList.end(); ++it)
+	{
+		if (is_abbrev(arg, MorphList[*it]->PadName().c_str()) || is_abbrev(arg, MorphList[*it]->Name().c_str()))
+			return *it;
+	}
+	return string();
+}
+
+string GetMorphIdByName(char *arg)
+{
+	for(MorphListType::const_iterator it = MorphList.begin(); it != MorphList.end(); ++it)
+	{
+		if (is_abbrev(arg, it->second->PadName().c_str()) || is_abbrev(arg, it->second->Name().c_str()))
+			return it->first;
+	}
+	return string();
+}
+
+void AnimalMorph::InitSkills(int value)
+{
+	for (CharSkillsType::iterator it = skills_.begin(); it != skills_.end();++it)
+	{
+		if (value)
+			it->second = value;
+	}
+	skills_[SKILL_MORPH]=value;
+};
+
+void AnimalMorph::InitAbils()
+{
+	int extraWis = ch_->get_inborn_wis() - MIN_WIS_FOR_MORPH;
+	wis_= ch_->get_inborn_wis();
+	if (extraWis > 0)
+	{
+		str_ = ch_->get_inborn_str() + extraWis * toStr_ /100;
+		dex_ = ch_->get_inborn_dex() + extraWis * toDex_ /100;
+		con_ = ch_->get_inborn_con() + extraWis * toCon_ /100;
+		cha_ = ch_->get_inborn_cha() + extraWis * toCha_ /100;
+		intel_ = ch_->get_inborn_int() + extraWis * toInt_ /100;
+	}else
+	{
+		str_ = ch_->get_inborn_str();
+		dex_ = ch_->get_inborn_dex();
+		con_ = ch_->get_inborn_con();
+		cha_ = ch_->get_inborn_cha();
+		intel_ = ch_->get_inborn_int();
+	}
+}
+
+void AnimalMorph::SetChar(CHAR_DATA *ch)
+{
+	ch_=ch;
+};
+
+ACMD(do_morph)
+{
+	if (IS_NPC(ch))
+		return;
+	if (!ch->get_skill(SKILL_MORPH))
+	{
+		send_to_char("Вы не знаете как.\r\n", ch);
+		return;
+	}
+
+	if (!*argument || ch->get_morphs_count() == 0)
+	{
+		ShowKnownMorphs(ch);
+		return;
+	}
+
+	skip_spaces(&argument);
+	one_argument(argument, arg);
+	
+
+	if (ch->is_morphed())
+	{
+		if (is_abbrev(arg, "назад"))
+		{
+			ch->reset_morph();
+			return;
+		}
+		send_to_char("Когти подстригите сначала...\r\n", ch);
+		return;
+	}
+
+	string morphId = FindMorphId(ch, arg);
+	if (morphId.empty())
+	{
+		send_to_char("Обернуться в ЭТО Вы не можете!\r\n", ch);
+		return;
+	}
+
+	string msg = "Хорошо, Вы попытаетесь обернуться "+MorphList[morphId]->PadName()+".\r\n";
+	send_to_char(msg, ch);
+	MorphPtr newMorph = MorphPtr(new AnimalMorph(*MorphList[morphId]));
+	ch->set_morph(newMorph);
+}
+
+void PrintAllMorphsList(CHAR_DATA *ch)
+{
+	send_to_char("Существующие формы: \r\n", ch);
+	for (MorphListType::const_iterator it = MorphList.begin();it != MorphList.end();++it)
+		send_to_char("   " + it->second->Name() + "\r\n", ch);
+}
+
+ACMD(do_morphset)
+{
+	CHAR_DATA * vict;
+
+	argument = one_argument(argument, arg);
+
+	if (!*arg)
+	{
+		send_to_char("Формат команды: morphset <имя игрока (только онлайн)> <название формы> \r\n", ch);
+		PrintAllMorphsList(ch);
+		return;
+	}
+	
+	if (!(vict = get_char_vis(ch, arg, FIND_CHAR_WORLD)))
+	{
+		send_to_char(NOPERSON, ch);
+		return;
+	}
+
+	skip_spaces(&argument);
+
+	string morphId = GetMorphIdByName(argument);
+	
+	if (morphId.empty())
+	{
+		send_to_char("Форма '"+string(argument)+"' не найдена. \r\n", ch);
+		PrintAllMorphsList(ch);
+		return;
+	}
+	
+	if (vict->know_morph(morphId))
+	{
+		send_to_char(string(vict->get_name()) + " уже знает эту форму. \r\n", ch);
+		return;
+	}
+
+	vict->add_morph(morphId);
+	
+	sprintf(buf2, "%s add morph %s to %s.", GET_NAME(ch), MorphList[morphId]->Name().c_str(), GET_NAME(vict));
+	mudlog(buf2, BRF, -1, SYSLOG, TRUE);
+	imm_log("%s add morph %s to %s.", GET_NAME(ch), MorphList[morphId]->Name().c_str(), GET_NAME(vict));
+
+	send_to_char("Вы добавили форму '"+ MorphList[morphId]->Name() +"' персонажу "+string(vict->get_name())+" \r\n", ch);
+}
+
+void load_morphs()
+{
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_file(LIB_MISC"morphs.xml");
+	if (!result)
+	{
+		snprintf(buf, MAX_STRING_LENGTH, "...%s", result.description());
+		mudlog(buf, CMP, LVL_IMMORT, SYSLOG, TRUE);
+		return;
+	}
+	pugi::xml_node node_list = doc.child("animalMorphs");
+	if (!node_list)
+	{
+		snprintf(buf, MAX_STRING_LENGTH, "...morphs list read fail");
+		mudlog(buf, CMP, LVL_IMMORT, SYSLOG, TRUE);
+		return;
+	}
+	MIN_WIS_FOR_MORPH = node_list.attribute("minWis").as_int();
+	
+	for (pugi::xml_node morph = node_list.child("morph");morph; morph = morph.next_sibling("morph"))
+	{
+		string id = string(morph.attribute("id").value());
+		if (id.empty())
+		{
+			snprintf(buf, MAX_STRING_LENGTH, "...morph id read fail");
+			mudlog(buf, CMP, LVL_IMMORT, SYSLOG, TRUE);
+			return;
+		}
+		DescListType descList;
+		for (pugi::xml_node desc = morph.child("description"); desc; desc = desc.next_sibling("description"))
+		{
+			DescNode node;
+			node.desc = string(desc.child_value());
+			node.fromLevel = desc.attribute("lvl").as_int();
+			descList.push_back(node);
+		}
+		CharSkillsType skills;
+		pugi::xml_node skillsList=morph.child("skills"); 
+		string name = morph.child_value("name");
+		string padName = morph.child_value("padName");
+		string coverDesc = morph.child_value("cover");		
+		string speech = morph.child_value("speech");
+		for (pugi::xml_node skill = skillsList.child("skill"); skill; skill = skill.next_sibling("skill"))
+		{
+			int skillNum = find_skill_num(skill.child_value());
+			if (skillNum != -1)
+				skills[skillNum]=0;//init-им скилы нулями, потом проставим при превращении
+			else
+			{
+				snprintf(buf, MAX_STRING_LENGTH, "...skills read fail for morph %s", name.c_str());
+				mudlog(buf, CMP, LVL_IMMORT, SYSLOG, TRUE);
+				return;
+			}
+		}
+		AnimalMorphPtr newMorph = AnimalMorphPtr(new AnimalMorph(id, name, padName, descList, skills, coverDesc, speech));
+		int toStr = atoi(morph.child_value("toStr"));
+		int toDex = atoi(morph.child_value("toDex"));
+		int toCon = atoi(morph.child_value("toCon"));
+		int toInt = atoi(morph.child_value("toInt"));
+		int toCha = atoi(morph.child_value("toCha"));
+		newMorph->SetAbilsParams(toStr, toDex, toCon, toInt, toCha);
+		MorphList[id] = newMorph;
+	}
+};
+
+void set_god_morphs(CHAR_DATA *ch)
+{
+	for (MorphListType::const_iterator it= MorphList.begin(); it != MorphList.end();++it)
+	{
+		ch->add_morph(it->first);
+	}
+}
+
+
---
+++ src/morph.hpp
@@ -0,0 +1,173 @@
+#ifndef MORPH_HPP_INCLUDED
+#define MORPH_HPP_INCLUDED
+
+#include <list> 
+#include <vector> 
+#include <boost/shared_ptr.hpp>
+#include "comm.h" 
+
+using namespace std;
+
+extern short MIN_WIS_FOR_MORPH;
+
+typedef struct 
+{
+	int fromLevel;
+	string desc;
+} DescNode;
+
+typedef list<DescNode> DescListType;
+
+class IMorph;
+class AnimalMorph;
+
+typedef boost::shared_ptr<IMorph> MorphPtr;
+typedef boost::shared_ptr<AnimalMorph> AnimalMorphPtr;
+
+typedef map<string, AnimalMorphPtr> MorphListType;
+
+typedef map<int, int> MorphSkillsList;
+
+class IMorph {
+public:
+	IMorph() {};
+	virtual ~IMorph() {};
+	virtual string Name()=0;
+	virtual string PadName()=0;
+	virtual string GetMorphDesc()=0;
+	virtual string GetMorphTitle()=0;
+	virtual void InitSkills(int value) {};
+	virtual void InitAbils() {};
+	virtual void SetAbilsParams(short toStr, short toDex, short toCon, short toInt, short toCha) {}; 
+	virtual void SetChar(CHAR_DATA *ch) {};
+	virtual string CoverDesc() {return "";};
+
+
+	virtual int GetStr() const =0;
+	virtual void SetStr(int str)=0;
+	virtual int GetIntel() const =0;
+	virtual void SetIntel(int intel)=0;
+	virtual int GetWis() const =0;
+	virtual void SetWis(int wis)=0;
+	virtual int GetDex() const =0;
+	virtual void SetDex(int dex)=0;
+	virtual int GetCha() const =0;
+	virtual void SetCha(int cha)=0;
+	virtual int GetCon() const =0;
+	virtual void SetCon(int con)=0;
+
+
+	virtual void set_skill(int skill_num, int percent)=0;
+	virtual int get_trained_skill(int skill_num)=0;
+
+};
+
+class NormalMorph : public IMorph
+{
+	NormalMorph (CHAR_DATA *ch) {ch_=ch;}
+public:
+	CHAR_DATA *ch_;
+
+	~NormalMorph () {};
+	static MorphPtr GetNormalMorph(CHAR_DATA *ch)
+	{
+		return MorphPtr(new NormalMorph(ch));
+	};
+
+	string GetMorphDesc();
+	string GetMorphTitle();
+	string Name() {return "Обычная";}
+	string PadName() {return "Человеком";};
+	void SetChar(CHAR_DATA *ch) {ch_=ch;};
+
+	void set_skill(int skill_num, int percent);
+	int get_trained_skill(int skill_num);
+
+	virtual int GetStr() const;
+	virtual void SetStr(int str);
+	virtual int GetIntel() const;
+	virtual void SetIntel(int intel);
+	virtual int GetWis() const;
+	virtual void SetWis(int wis);
+	virtual int GetDex() const;
+	virtual void SetDex(int dex);
+	virtual int GetCha() const;
+	virtual void SetCha(int cha);
+	virtual int GetCon() const;
+	virtual void SetCon(int con);
+
+};
+
+class AnimalMorph : public IMorph
+{
+	CHAR_DATA *ch_;
+	string id_;
+	string name_;
+	string padName_;
+	DescListType descList_;
+	MorphSkillsList skills_;
+	string coverDesc_;
+	string speech_;
+	short toStr_;
+	short toDex_;
+	short toCon_;
+	short toInt_;
+	short toCha_;
+	int str_;
+	int intel_;
+	int wis_;
+	int dex_;
+	int cha_;
+	int con_;
+public:
+	AnimalMorph(string id, string name, string padName, DescListType descList, MorphSkillsList skills, string coverDesc, string speech) : 
+		id_(id),
+		name_(name), 
+		padName_(padName),
+		descList_(descList),
+		skills_(skills),
+		coverDesc_(coverDesc),
+		speech_(speech)
+	{};
+	
+	~AnimalMorph() {};
+
+	string GetMorphDesc();
+	string Name() {return name_;}
+	string PadName() {return padName_;}
+	string GetMorphTitle();
+	string CoverDesc() {return coverDesc_;}
+	void InitSkills(int value);
+	void InitAbils();
+	void SetChar(CHAR_DATA *ch);
+	void SetAbilsParams(short toStr, short toDex, short toCon, short toInt, short toCha) 
+	{
+		toStr_ = toStr;
+		toDex_ = toDex;
+		toCon_ = toCon;
+		toCha_ = toCha;
+		toInt_ = toInt;
+	}; 
+
+
+	virtual int GetStr() const {return str_;}
+	virtual void SetStr(int str) {str_=str;}
+	virtual int GetIntel() const {return intel_;}
+	virtual void SetIntel(int intel) {intel_=intel;}
+	virtual int GetWis() const {return wis_;}
+	virtual void SetWis(int wis) {wis_=wis;}
+	virtual int GetDex() const {return dex_;}
+	virtual void SetDex(int dex) {dex_=dex;}
+	virtual int GetCha() const {return cha_;}
+	virtual void SetCha(int cha) {cha_=cha;}
+	virtual int GetCon()  const {return con_;}
+	virtual void SetCon(int con) {con_=con;}
+
+	void set_skill(int skill_num, int percent);
+	int get_trained_skill(int skill_num);
+};
+
+
+void load_morphs();
+void set_god_morphs(CHAR_DATA *ch);
+#endif // MORPH_HPP_INCLUDED
