312756f180926752a053dbfd0ec8cab756d2d865
Sventovit <sventovit@bk.ru>
2012-05-25 01:27:48 +0700

    preparation scripts

---
+++ winprep.bat
@@ -0,0 +1,36 @@
+@echo off
+
+@md lib.template
+@md lib.template\etc
+md lib.template\etc\board
+@md lib.template\plralias
+md lib.template\plralias\A-E
+md lib.template\plralias\F-J
+md lib.template\plralias\K-O
+md lib.template\plralias\P-T
+md lib.template\plralias\U-Z
+md lib.template\plralias\ZZZ
+@md lib.template\plrobjs
+md lib.template\plrobjs\A-E
+md lib.template\plrobjs\F-J
+md lib.template\plrobjs\K-O
+md lib.template\plrobjs\P-T
+md lib.template\plrobjs\U-Z
+md lib.template\plrobjs\ZZZ
+@md lib.template\plrs
+md lib.template\plrs\A-E
+md lib.template\plrs\F-J
+md lib.template\plrs\K-O
+md lib.template\plrs\P-T
+md lib.template\plrs\U-Z
+md lib.template\plrs\ZZZ
+@md lib.template\plrstuff
+@md lib.template\plrstuff\depot
+md lib.template\plrstuff\depot\A-E
+md lib.template\plrstuff\depot\F-J
+md lib.template\plrstuff\depot\K-O
+md lib.template\plrstuff\depot\P-T
+md lib.template\plrstuff\depot\U-Z
+md lib.template\plrstuff\depot\ZZZ
+md lib.template\stat
+
