5ea0ccb62dc8aefc6b1476bcec6aea300cf694db
PovelitelDvuhgolovogoZmeya <gedooru@gmail.com>
2014-09-28 17:56:07 +0300

    Маленькая правка

--- src/act.informative.cpp
+++ src/act.informative.cpp
@@ -358,7 +358,7 @@ const char *diag_obj_timer(const OBJ_DATA * obj)
 		{
 			return "Прототип предмета имеет нулевой таймер!\r\n";
 		}
-		if (prot_timer == UTIMER)
+		if (obj->get_timer() == UTIMER)
 		    return "Нерушим";
 		int tm = (obj->get_timer() * 100 / prot_timer);
 		return print_obj_state(tm);
