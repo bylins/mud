5668767fecfdcb612ce7f87ebe7db7641968c086
bylins <devnull@localhost>
2015-08-29 11:57:47 +0300

    Имплементора теперь нельзя помутить

--- src/act.wizard.cpp
+++ src/act.wizard.cpp
@@ -248,7 +248,7 @@ int set_punish(CHAR_DATA * ch, CHAR_DATA * vict, int punish , char * reason , lo
 		return 0;
 	}
 
-	if ((GET_LEVEL(vict) >= LVL_IMMORT && !IS_IMPL(ch)) || PRF_FLAGGED(vict, PRF_CODERINFO))
+	if ((GET_LEVEL(vict) >= LVL_IMMORT && !IS_IMPL(ch)) || PRF_FLAGGED(vict, PRF_CODERINFO) || IS_IMPL(vict))
 	{
 		send_to_char("Кем вы себя возомнили?\r\n", ch);
 		return 0;
