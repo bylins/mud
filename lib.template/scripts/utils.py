5408aa34bf3e0319c07affb986bd7f9610ed3a68
Anton Gorev <kvirund@gmail.com>
2015-12-05 00:48:37 -0500

    Fixed utils.py (it was tried to print stacktrace of exception when no any exceptions were occurred).

--- lib.template/scripts/utils.py
+++ lib.template/scripts/utils.py
@@ -5,9 +5,11 @@ import traceback
 import mud, constants
 
 def log_sys(msg):
-    mud.log(msg + "\n" + traceback.format_exc(), channel=constants.SYSLOG)
+    try:
+        stacktrace = traceback.format_exc()
+    except AttributeError:
+        stacktrace = "<no any exceptions were occurred>"
+    mud.log(msg + "\n" + stacktrace, channel=constants.SYSLOG)
 	
 def reg_cmd(cmd_text, command,  position_min, level_min, unhide_percent):
-	mud.register_global_command(cmd_text, cmd_text.encode("koi8-r"), command, position_min, level_min, unhide_percent)
-	
-    
+    mud.register_global_command(cmd_text, cmd_text.encode("koi8-r"), command, position_min, level_min, unhide_percent)
