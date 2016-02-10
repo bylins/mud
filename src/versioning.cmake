dab2be5a6a363f6acf257270e4aeaf510bc4b56a
Anton Gorev <kvirund@gmail.com>
2016-02-09 19:58:47 -0600

    Force invalidate version.cpp file to force rebuild this file even if it was not changed: it is required to update date and time of build.

--- src/versioning.cmake
+++ src/versioning.cmake
@@ -4,5 +4,6 @@ else ()
     set(REVISION "<undefined>")
 endif ()
 configure_file(${VERSION_INPUT} ${VERSION_OUTPUT})
+execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${VERSION_OUTPUT})	# invalidate output to force rebuild this source file even if it was not changed.
 
 # vim: set ts=4 sw=4 ai tw=0 et syntax=cmake :
