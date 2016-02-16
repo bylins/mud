8a803f5cbc4b19352459b3d1dfa919a92268911d
Stribog_ <devnull@localhost>
2016-02-16 17:30:24 +0300

    удалить себя можно до 6 морта, тать и наем сникают по старому

--- src/act.movement.cpp
+++ src/act.movement.cpp
@@ -262,7 +262,10 @@ int skip_sneaking(CHAR_DATA * ch, CHAR_DATA * vict)
 		}
 		else if (affected_by_spell(ch, SPELL_SNEAK))
 		{
-			percent = number(1, 82 + (GET_REAL_INT(vict) * vict->get_role(MOB_ROLE_BOSS) ? 3 : 1) + (GET_LEVEL(vict) > 30 ? GET_LEVEL(vict) : 0));
+			if (can_use_feat(ch, WRIGGLER_FEAT)) //тать или наем
+				percent = number(1, 102 + GET_REAL_INT(vict));
+			else
+				percent = number(1, 102 + (GET_REAL_INT(vict) * vict->get_role(MOB_ROLE_BOSS) ? 3 : 1) + (GET_LEVEL(vict) > 30 ? GET_LEVEL(vict) : 0));
 			prob = calculate_skill(ch, SKILL_SNEAK, percent, vict);
 			if (percent > prob)
 			{
