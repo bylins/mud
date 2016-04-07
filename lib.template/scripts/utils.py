677a676c6b6962a19238194119c3163b23c21b53
Anton Gorev <kvirund@gmail.com>
2016-04-07 18:01:24 +0300

    Backed out changes in script files: error must be at some other place.

--- lib.template/scripts/utils.py
+++ lib.template/scripts/utils.py
@@ -5,7 +5,8 @@ import traceback
 import mud, constants
 
 def log_sys(msg):
-    mud.log(msg + "\n" + traceback.format_exc(), channel=constants.SYSLOG)
+    s = msg + "\n" + trace
+    mud.log(s.encode("koi8-r"), channel=constants.SYSLOG)
 	
 def reg_cmd(cmd_text, command,  position_min, level_min, unhide_percent):
 	mud.register_global_command(cmd_text, cmd_text.encode("koi8-r"), command, position_min, level_min, unhide_percent)
