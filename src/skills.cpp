4a7ea8596c7569d9540c246a013ebaed773ee044
Fercan <devnull@localhost>
2016-04-26 19:03:22 +0700

    Привел к старым изменениям

--- src/skills.cpp
+++ src/skills.cpp
@@ -683,7 +683,8 @@ int calculate_skill(CHAR_DATA * ch, int skill_no, CHAR_DATA * vict)
 	case SKILL_MIGHTHIT: // богатырский молот
 		//victim_sav = GET_SAVE(vict, SAVING_STABILITY) - dex_bonus(GET_REAL_CON(vict));
 		victim_sav = -GET_REAL_SAVING_STABILITY(vict);
-		bonus = size + dex_bonus(GET_REAL_STR(ch));
+		//bonus = size + dex_bonus(GET_REAL_STR(ch));
+		bonus = dex_bonus(GET_REAL_STR(ch));
 
 		if (IS_NPC(vict))
 			victim_modi -= (size_app[GET_POS_SIZE(vict)].shocking)/2;
