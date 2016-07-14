1d57603c3bfb0d2e0941a4af0357b90e5ccc1efc
Polel <edpod@ukr.net>
2016-07-14 23:31:34 +0300

    new magic

--- src/spells.h
+++ src/spells.h
@@ -359,7 +359,8 @@ enum ESpell
 	SPELL_RECOVERY = 205,	// исцеление нежити
 	SPELL_MASS_RECOVERY = 206,	// масовое исцеление нежити
 	SPELL_AURA_EVIL = 207,	// аура зла для всей нежити в группе
-	SPELLS_COUNT = SPELL_AURA_EVIL	// Counter corresponds to the last value because we count spells from 1.
+	SPELL_MENTAL_SHADOW = 208,	// ментальная тень
+	SPELLS_COUNT = SPELL_MENTAL_SHADOW	// Counter corresponds to the last value because we count spells from 1.
 };
 
 typedef std::array<const char*, SPELLS_COUNT + 1> spell_wear_off_msg_t;
