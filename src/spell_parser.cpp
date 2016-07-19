f02daec69abb92eb47a1efc26c7a52a232bc5f14
Stribog_ <devnull@localhost>
2016-07-19 20:59:05 +0300

    новая формула учета умения магии на скорость мема

--- src/spell_parser.cpp
+++ src/spell_parser.cpp
@@ -1590,9 +1590,6 @@ inline int spell_create_level(CHAR_DATA * ch, int spellnum)
 
 float koef_skill_magic(int percent_skill)
 {
-	// вариант А
-	//return percent_skill / 100;
-	// вариант Б
 	return percent_skill / 100 + 1;
 
 	return 0;
@@ -1629,8 +1626,7 @@ int mag_manacost(CHAR_DATA * ch, int spellnum)
 			mana_cost = mana_cost * (100 - MIN(99, abs(SpINFO.class_change[(int) GET_CLASS(ch)][(int) GET_KIN(ch)]))) / 100;
 		else
 			mana_cost = mana_cost * 100 / (100 - MIN(99, abs(SpINFO.class_change[(int) GET_CLASS(ch)][(int) GET_KIN(ch)])));
-		return mana_cost / koef_skill_magic(get_magic_skill_number_by_spell(spellnum));
-//		return mana_cost / koef_skill_magic(ch->get_skill(get_magic_skill_number_by_spell(spellnum))); пока не родим формулу
+		return mana_cost / ( 12 - koef_skill_magic(ch->get_skill(get_magic_skill_number_by_spell(spellnum)))) / 12;
 	};
 	return 9999;
 	//#define GET_MANA_COST(ch,spellnum)      (mana_cost_cs[(int)GET_LEVEL(ch)][spell_create[spellnum].runes.krug-1])
