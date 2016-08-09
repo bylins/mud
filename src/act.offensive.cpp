b31ae586bb2d2bf7ef93317eed74ea765e7b99ea
Polel <edpod@ukr.net>
2016-08-09 14:28:11 +0300

    улучшение чармиса купца

--- src/act.offensive.cpp
+++ src/act.offensive.cpp
@@ -908,8 +908,8 @@ void go_bash(CHAR_DATA * ch, CHAR_DATA * vict)
 		return;
 	}
 
-	if (!(IS_NPC(ch) ||	// моб
-			GET_EQ(ch, WEAR_SHIELD) ||	// есть щит
+	//if (!(IS_NPC(ch) ||	// моб
+	if (!(GET_EQ(ch, WEAR_SHIELD) ||	// есть щит
 			IS_IMMORTAL(ch) ||	// бессмертный
 			GET_MOB_HOLD(vict) ||	// цель захолжена
 			GET_GOD_FLAG(vict, GF_GODSCURSE)	// есть спецфлаг
