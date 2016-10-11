eee25a643e76de546816add470f7ac8a0f739944
Stribog_ <devnull@localhost>
2016-10-11 10:13:24 +0300

    убрал спам в сислог

--- src/act.item.cpp
+++ src/act.item.cpp
@@ -1686,8 +1686,8 @@ MeatMapping::MeatMapping()
 std::unordered_map<obj_vnum, obj_vnum>::key_type MeatMapping::random_key() const
 {
 	const auto index = number(1 , static_cast<int>(size() - 1));
-	sprintf(buf, "Размер мясного массива %d выпал предмет под номером %d с vnum %d", static_cast<int>(size()), index, m_index_mapping[index]);
-	mudlog(buf, NRM, LVL_IMMORT, SYSLOG, TRUE);
+//	sprintf(buf, "Размер мясного массива %d выпал предмет под номером %d с vnum %d", static_cast<int>(size()), index, m_index_mapping[index]);
+//	mudlog(buf, NRM, LVL_IMMORT, SYSLOG, TRUE);
 	return m_index_mapping[index];
 }
 
