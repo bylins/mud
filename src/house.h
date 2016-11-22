f0fa997427d4f7b2e7cc91391ed18bb16ece7399
Anton Gorev <kvirund@gmail.com>
2016-11-21 20:38:50 -0600

    Поправил ошибку компиляции под Linux.

--- src/house.h
+++ src/house.h
@@ -345,7 +345,7 @@ private:
 
 	static void HcontrolBuild(CHAR_DATA * ch, std::string & buffer);
 	static void HcontrolDestroy(CHAR_DATA * ch, std::string & buffer);
-	static void DestroyClan(Clan::shared_ptr& clan);
+	static void DestroyClan(Clan::shared_ptr clan);
 	static void hcontrol_title(CHAR_DATA *ch, std::string &text);
 	static void hcontrol_rank(CHAR_DATA *ch, std::string &text);
 	static void hcontrol_exphistory(CHAR_DATA *ch, std::string &text);
