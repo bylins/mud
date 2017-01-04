bdae6d41465e0b48c3e8c1d431e740cafe1bc503
bodrich <gedooru@gmail.com>
2017-01-04 18:14:37 +0500

    Правка баги с бордами, правка баги с триггерами

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "comm.h"
-
-int main(const int argc, char **argv)
-{
-	return main_function(argc, argv);
-}
-
-// vim: ts=4 sw=4 tw=0 noet syntax=cpp :
--- src/spell_parser.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __SPELL_PARSER_HPP__
-#define __SPELL_PARSER_HPP__
-
-#include "skills.h"
-
-ESkill find_skill_num(const char *name);
-
-#endif // __SPELL_PARSER_HPP__
