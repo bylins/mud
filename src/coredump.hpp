7f638e6aeeee8ce20a72aca8b7f5702be4a439a1
Anton Gorev <kvirund@gmail.com>
2017-01-13 18:07:04 -0600

    Добавил снятие кордампа с текущего процесса при возникновении неуловимой ошибки.

---
+++ src/coredump.hpp
@@ -0,0 +1,28 @@
+#ifndef __COREDUMP_HPP__
+#define __COREDUMP_HPP__
+
+#ifndef WIN32
+#include <unistd.h>
+
+#include <cstdlib>
+#endif
+
+namespace debug
+{
+	void coredump()
+	{
+#ifndef WIN32
+		pid_t pid = fork();
+		if (0 == pid)
+		{
+			abort();
+		}
+		int result = 0;
+		wait(&result);
+#endif
+	}
+}
+
+#endif
+
+// vim: ts=4 sw=4 tw=0 noet syntax=cpp :
