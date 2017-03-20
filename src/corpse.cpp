5abaebe0bc05eced80866a015f91b567b7fcad62
Stribog <stribog@bylins.su>
2017-03-20 10:08:37 +0300

    лошади вне фридропа

--- src/corpse.cpp
+++ src/corpse.cpp
@@ -351,6 +351,8 @@ int get_obj_to_drop(DropListType::iterator &i)
  */
 bool check_mob(OBJ_DATA *corpse, CHAR_DATA *mob)
 {
+	if (MOB_FLAGGED(mob, MOB_MOUNTING))
+		return false;
 	for (size_t i = 0; i < tables_drop.size(); i++)
 	{
 		if (tables_drop[i].check_mob(GET_MOB_VNUM(mob)))
@@ -372,7 +374,7 @@ bool check_mob(OBJ_DATA *corpse, CHAR_DATA *mob)
 	for (DropListType::iterator i = drop_list.begin(), iend = drop_list.end(); i != iend; ++i)
 	{ int day = time_info.month * DAYS_PER_MONTH + time_info.day + 1;
 		if (GET_LEVEL(mob) >= i->mob_lvl 				   
-		    && (!i->max_mob_lvl
+		    &&	(!i->max_mob_lvl
 				|| GET_LEVEL(mob) <= i->max_mob_lvl) 		// моб в диапазоне уровней
 		    && ((i->race_mob < 0)
 				|| (GET_RACE(mob) == i->race_mob)
