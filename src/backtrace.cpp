f8ee9cfb74c0fac86602ccc506e3d2318b93d90c
Stribog <stribog@bylins.su>
2017-06-02 13:00:39 +0300

    откат бактрейса

--- src/backtrace.cpp
+++ src/backtrace.cpp
@@ -15,7 +15,7 @@
 
 #else	// _WIN32
 
-//#include <execinfo.h>
+#include <execinfo.h>
 
 #endif
 
@@ -23,9 +23,47 @@ namespace debug
 {
 	constexpr unsigned short MAX_STACK_SIZE = USHRT_MAX;
 
+#ifdef _WIN32
+	void win32_backtrace(FILE* file)
+	{
+		constexpr size_t MAX_NAME_LENGTH = 1024u;
+
+		const auto process = GetCurrentProcess();
+		SymInitialize(process, nullptr, TRUE);
+		void* stack[MAX_STACK_SIZE];
+		const auto frames = CaptureStackBackTrace(0, MAX_STACK_SIZE, stack, nullptr);
+		SYMBOL_INFO* symbol = (SYMBOL_INFO *) calloc(sizeof(SYMBOL_INFO) + MAX_NAME_LENGTH*sizeof(char), 1);
+		symbol->MaxNameLen = MAX_NAME_LENGTH - 1;
+		symbol->SizeOfStruct = sizeof(*symbol);
+		for (auto i = 0; i < frames; ++i)
+		{
+			SymFromAddr(process, reinterpret_cast<DWORD64>(stack[i]), 0, symbol);
+			fprintf(file, "%02d: %s at 0x%08llx\n",
+				frames - i - 1, symbol->Name, static_cast<unsigned long long>(symbol->Address));
+		}
+		free(symbol);
+	}
+#else
+	void linux_backtrace(FILE* file)
+	{
+		void* stack[MAX_STACK_SIZE];
+		const auto frames = ::backtrace(stack, MAX_STACK_SIZE);
+		backtrace_symbols_fd(stack, frames, fileno(file));
+	}
+#endif
+
 	void backtrace(FILE* file)
 	{
-		return;
+		if (nullptr == file)
+		{
+			return;
+		}
+
+#ifdef _WIN32
+		win32_backtrace(file);
+#else
+		linux_backtrace(file);
+#endif
 	}
 }
 
