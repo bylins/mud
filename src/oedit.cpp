48df3e55701833571dcce31f5416341361834f74
dethman <dehtman@yandex.ru>
2017-10-06 18:08:29 +0300

    create test

--- src/oedit.cpp
+++ src/oedit.cpp
@@ -165,13 +165,9 @@ void olc_update_object(int robj_num, OBJ_DATA *obj, OBJ_DATA *olc_proto)
 	// Копируем информацию из прототипа
 	*obj = *olc_proto;
 	
-	//Восстанавливаем падежи
-	if (obj->get_is_rename()) {
+	//Восстанавливаем падежи если объект поренеймлен
+	if (obj->get_is_rename())
 		obj->copy_name_from(&tmp);
-		//obj->set_PName(i, obj_pad);
-		//obj->set_short_description(obj_pad);
-		//obj->set_aliases(obj_pad); // ставим алиасы
-	}
 
 	obj->clear_proto_script();
 	// Восстанавливаю игровую информацию
