c82a98d3b0849d0d7da0ee2ab450de74030dbfd3
Anton Gorev <kvirund@gmail.com>
2018-11-08 15:04:02 -0600

    Избавился ещё от одного изменения той же глобальной переменной.

--- src/logger.cpp
+++ src/logger.cpp
@@ -12,14 +12,6 @@
 // Файл для вывода
 FILE *logfile = nullptr;
 
-void log(const char *format, ...)
-{
-	va_list args;
-	va_start(args, format);
-	vlog(format, args);
-	va_end(args);
-}
-
 std::size_t vlog_buffer(char* buffer, const std::size_t buffer_size, const char* format, va_list args)
 {
 	std::size_t result = ~0u;
@@ -95,7 +87,7 @@ void vlog(const char *format, va_list args, FILE* logfile)
 
 void vlog(const char *format, va_list args)
 {
-	vlog(format, args, logfile);
+	vlog(format, args, ::logfile);
 }
 
 void vlog(const EOutputStream steam, const char* format, va_list rargs)
@@ -103,9 +95,25 @@ void vlog(const EOutputStream steam, const char* format, va_list rargs)
 	va_list args;
 	va_copy(args, rargs);
 
-	const auto logfile = runtime_config.logs(steam).handle();
-	vlog(format, args, logfile);
+	const auto log = runtime_config.logs(steam).handle();
+	vlog(format, args, log);
 
+	va_end(args);
+}
+
+void log(FILE* log, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vlog(format, args, log);
+	va_end(args);
+}
+
+void log(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vlog(format, args, ::logfile);
 	va_end(args);
 }
 
@@ -238,9 +246,8 @@ void mudlog(const char *str, int type, int level, EOutputStream channel, int fil
 
 	if (file)
 	{
-		logfile = runtime_config.logs(channel).handle();
-		log("%s", str);
-		logfile = runtime_config.logs(SYSLOG).handle();
+		const auto log = runtime_config.logs(channel).handle();
+		::log(log, "%s", str);
 	}
 
 	if (level < 0)
