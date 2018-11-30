bylins/mud

commit 218258d329786a88f5a3f5ed8980646c363abe25

fix leak in init_guilds()
This only manifests on test server (with incomplete world) because some guild masters are missing. If all guild masters are missing for the given guild, allocated list of skills is not released.

src/spec_procs.cpp
@@ -776,6 +776,9 @@ void init_guilds(void)
 				 || !strn_cmp(line, "многоклассовая", strlen(line)))
 		{
 			type = 2;
+			// free if we still own it from previous iteration due to some error
+			if (poly_guild)
+				free(poly_guild);
 			poly_guild = NULL;
 			pgcount = 0;
 		}
@@ -841,6 +844,7 @@ void init_guilds(void)
 					(poly_guild + pgcount)->spell_no = -1;
 					(poly_guild + pgcount)->level = -1;
 					guild_poly_info[GUILDS_POLY_USED] = poly_guild;
+					poly_guild = NULL; // null it to release ownership
 					GUILDS_POLY_USED++;
 				}
 				else
@@ -965,6 +969,9 @@ void init_guilds(void)
 		}
 	}
 	fclose(magic);
+	// free if we still own it
+	if (poly_guild)
+		free(poly_guild);
 	return;
 }
 

commit ac394c55476292471c4407ec9e626097fbe85f8d

fix regression in init_guilds()
fix bug introduced in 218258d329786a88f5a3f5ed8980646c363abe25

src/spec_procs.cpp
@@ -791,8 +791,8 @@ void init_guilds(void)
 				continue;
 			}
 
-			if (!((type == 1 || type == 11) && mono_guild.learn_info) &&
-					!((type == 2 || type == 12) && poly_guild))
+			if (!((type == 1 && mono_guild.learn_info) || type == 11) &&
+					!((type == 2 && poly_guild) || type == 12)
 			{
 				log("WARNING: Can't define guild info for master %s. Skipped.", line1);
 				continue;