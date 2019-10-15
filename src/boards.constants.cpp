4da6d04231469b6daf46f38d76080ab8e235d028
Bylins <stribog@bylins.su>
2019-10-15 17:05:16 +0300

    ÐÒÁ×ËÁ ÄÏÓËÉ ËÏÄÅÒ

--- src/boards.constants.cpp
+++ src/boards.constants.cpp
@@ -5,7 +5,7 @@ namespace Boards
 	namespace constants
 	{
 		const char *OVERFLOW_MESSAGE = "Да, набросали всего столько, что файл переполнен. Напомните об этом богам!\r\n";
-		const char* CHANGELOG_FILE_NAME = "../bin/changelog";
+		const char* CHANGELOG_FILE_NAME = "../build/changelog";
 
 		namespace loader_formats
 		{
