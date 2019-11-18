7f04fdb4fe41d782d267d2768724c529285b601c
Sventovit <Sventovit@bk.ru>
2019-11-18 15:48:57 +0000

    Cooldowns - start

---
+++ src/char.cooldowns.cpp
@@ -0,0 +1,19 @@
+
+/*
+	Реализация кулдаунов для умений.
+*/
+
+#include "char.cooldowns.hpp"
+#include "skills.h"
+
+class AbilityCooldownType {
+    short globalCoolDown;
+    map<ESkill, short> abilityCooldowns;
+
+public:
+	void setGlobalCooldown(short cooldown);
+	void setAbilityCooldown(ESkill ability, short cooldown);
+	short getAbilityCooldown(ESkill ability);
+};
+
+// vim: ts=4 sw=4 tw=0 noet syntax=cpp :
