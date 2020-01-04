84c29d8dce060d561116ed86bd15eeb9af63c7c0
Bylins <stribog@bylins.su>
2020-01-04 09:56:47 +0300

    мелочи

--- backup_lib.sh
+++ backup_lib.sh
@@ -2,7 +2,7 @@
 
 DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 
-tar  -cvf ${DIRECTORY}/backup/lib.$(date +%d%m%y).tgz --exclude="mirror" ${DIRECTORY}/lib 
+tar  -cjf ${DIRECTORY}/backup/lib.$(date +%d%m%y).tgz --exclude="mirror" ${DIRECTORY}/lib 
 gdrive upload ${DIRECTORY}/backup/lib.$(date +%d%m%y).tgz
 
 # vim: set ts=4 sw=4 tw=0 noet :
