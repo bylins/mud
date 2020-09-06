e9aff80ec3ef8156ac1823bb1a863d1d5ed49803
bikbai <abomination.of.god@gmai.com>
2020-09-07 00:57:47 +0300

    Рефакторинг - разнес боевые умения по файликам. Причесал работу с конями - лишние функции и всё такое. Убрал возможность агро по одной букве у стаба чармисом.

---
+++ src/cmd/retreat.h
@@ -0,0 +1,8 @@
+#ifndef BYLINS_RETREAT_H
+#define BYLINS_RETREAT_H
+
+#include "chars/char.hpp"
+
+void do_retreat(CHAR_DATA *ch, char* /*argument*/, int/* cmd*/, int/* subcmd*/);
+
+#endif //BYLINS_RETREAT_H
---
+++ src/fightsystem/assist.h
@@ -0,0 +1,8 @@
+#ifndef BYLINS_ASSIST_H
+#define BYLINS_ASSIST_H
+
+#include "chars/char.hpp"
+
+void do_assist(CHAR_DATA *ch, char *argument, int/* cmd*/, int/* subcmd*/);
+
+#endif //BYLINS_ASSIST_H
