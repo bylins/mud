8432667ae648d9d9790c5df794095184082de7e1
bikbai <abomination.of.god@gmai.com>
2020-09-20 02:06:19 +0300

    Обвязка сообщения в телеграм готова.

---
+++ src/cmd/telegram.cpp
@@ -0,0 +1,6 @@
+#include "telegram.h"
+
+
+void do_telegram(CHAR_DATA *ch, char *argument, int, int){
+
+}
---
+++ src/cmd/telegram.h
@@ -0,0 +1,8 @@
+#ifndef BYLINS_TELEGRAM_H
+#define BYLINS_TELEGRAM_H
+
+#include "chars/char.hpp"
+
+void do_telegram(CHAR_DATA *ch, char *argument, int, int);
+
+#endif //BYLINS_TELEGRAM_H
