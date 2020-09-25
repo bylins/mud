10bf796242274d2ef2ecc226cc42f39073d5b59a
Bylins <stribog@bylins.su>
2020-09-25 11:51:00 +0300

    правка справки

--- backup_plrs.sh
+++ backup_plrs.sh
@@ -3,7 +3,7 @@
 DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 PLRS_BACKUP_PREFIX=plrs
 
-tar -cjf ${DIRECTORY}/backup/${PLRS_BACKUP_PREFIX}.$(date +%d%m%y-%H%M%S).tgz ${DIRECTORY}/lib/{plrs,plrobjs,plrstuff,plrvars,etc,plralias}
+tar -cpjf ${DIRECTORY}/backup/${PLRS_BACKUP_PREFIX}.$(date +%d%m%y-%H%M%S).tgz ${DIRECTORY}/lib/{plrs,plrobjs,plrstuff,plrvars,etc,plralias}
 find ${DIRECTORY}/backup/ -name "${PLRS_BACKUP_PREFIX}.*" -atime 2 -exec rm '{}' \;
 
 # vim: set ts=4 sw=4 tw=0 noet :
