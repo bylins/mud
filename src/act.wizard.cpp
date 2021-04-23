a8624dfa33144934649a96b89f4be0e4dc6539ab
Bylins <stribog@bylins.su>
2021-04-23 15:35:43 +0300

    первая фиксация см #912

--- src/act.wizard.cpp
+++ src/act.wizard.cpp
@@ -2504,8 +2504,20 @@ void inspecting()
 		if (*buf1 || mail_found)
 		{
 			const auto& player = player_table[it->second->pos];
+			CHAR_DATA * victim;
+				for (const auto i : character_list) {
+                                if (IS_NPC(i))
+                                        continue;
+                                if (GET_IDNUM(i) == player.id()) {
+                                        victim = i.get();
+                                        break;
+                                }
+                        }
+                        if(victim) {
+                                sprinttype(victim->get_class(), pc_class_types, smallBuf);
+                        }
 			mytime = player_table[it->second->pos].last_logon;
-			sprintf(buf, "--------------------\r\nИмя: %s%-12s%s e-mail: %s&S%-30s&s%s Last: %s. Level %d/%d.\r\n",
+			sprintf(buf, "--------------------\r\nИмя: %s%-12s%s e-mail: %s&S%-30s&s%s Last: %s. Level %d, Remort %d, Проф: %s.\r\n",
 				(is_online ? CCGRN(ch, C_SPR) : CCWHT(ch, C_SPR)),
 				player.name(),
 				CCNRM(ch, C_SPR),
@@ -2513,7 +2525,7 @@ void inspecting()
 				player.mail,
 				(mail_found ? CCNRM(ch, C_SPR) : ""),
 				rustime(localtime(&mytime)),
-				player.level, player.remorts);
+				player.level, player.remorts, smallBuf);
 			it->second->out += buf;
 			it->second->out += buf2;
 			it->second->out += buf1;
