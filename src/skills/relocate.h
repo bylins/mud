b58d8d96635bdc6768e20c881fc77f4c559732ec
Sventovit <Sventovit@bk.ru>
2021-04-25 09:23:10 +0000

    Level check for groupmates disabled

---
+++ src/skills/relocate.h
@@ -0,0 +1,10 @@
+#ifndef BYLINS_RELOCATE_H
+#define BYLINS_RELOCATE_H
+
+class CHAR_DATA;
+
+void do_relocate(CHAR_DATA *ch, char *argument, int/* cmd*/, int/* subcmd*/);
+
+#endif //BYLINS_RELOCATE_H
+
+// vim: ts=4 sw=4 tw=0 noet syntax=cpp :
