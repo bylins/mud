4dd462f087c4d6afa478533b875b7fe9f30aac88
Svjatovit <52343523+Svjatovit@users.noreply.github.com>
2021-04-28 14:48:40 +0700

    Hire overflow - fixed (probably) (#923)
    * Hire overflow - fixed (probably)
    
    * Additional fixes

--- src/cmd.wiz/godtest.cpp
+++ src/cmd.wiz/godtest.cpp
@@ -1,15 +1,15 @@
 #include "godtest.h"
 
 #include "../chars/char.hpp"
-#include <sstream>
-#include <iomanip>
 #include "../modify.h"
 
+#include <sstream>
+#include <iomanip>
 
 // This is test command for different testings
 void do_godtest(CHAR_DATA *ch, char* /*argument*/, int /* cmd */, int /* subcmd */) {
 	std::stringstream buffer;
-	buffer << "В настоящий момент процiдурка пуста.\r\nЕсли вам хочется что-то godtest, придется ее реализовать." << std::endl;
+	buffer << "В настоящий момент процiдурка пуста.\r\nЕсли вам хочется что-то godtest - придется ее реализовать." << std::endl;
 	page_string(ch->desc, buffer.str());
 }
 
