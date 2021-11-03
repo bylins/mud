3a0d08af56574295b6aa6c8d10a463d995159414
Bylins <stribog@bylins.su>
2021-11-03 23:27:57 +0300

    виснем на удалении 1 спелла при нескольких временных см #1166

--- src/modify.cpp
+++ src/modify.cpp
@@ -1047,24 +1047,19 @@ void do_skillset(CHAR_DATA *ch, char *argument, int/* cmd*/, int/* subcmd*/) {
 			spell >= 0 ? spell_info[spell].name : skill_info[skill].name, value);
 	mudlog(buf2, BRF, LVL_IMMORT, SYSLOG, TRUE);
 	if (spell >= 0 && spell <= SPELLS_COUNT) {
-
-		if (value == 0) {
-			if (IS_SET(GET_SPELL_TYPE(vict, spell), SPELL_TEMP)) {
-//				send_to_char(ch, "Вы проверяете стирание темп .\r\n");
-				Temporary_Spells::add_spell(vict, spell, time(0), 0);
-/*				for (auto it = vict->temp_spells.begin(); it != vict->temp_spells.end();) 
-					if (it->first == spell) {
-						it->second.set_time = time(0);
-						it->second.duration = 0;
-						send_to_char(ch, "Вы убрали .\r\n");
-						it = vict->temp_spells.erase(it);
-					} */
+		if (value == 0 && IS_SET(GET_SPELL_TYPE(vict, spell), SPELL_TEMP)) {
+			send_to_char(ch, "Вы стирание темп .\r\n");
+			for (auto it = vict->temp_spells.begin(); it != vict->temp_spells.end();) {
+				if (it->second.spell == spell) {
+//					REMOVE_BIT(GET_SPELL_TYPE(vict, it->first), SPELL_TEMP);
+					it = vict->temp_spells.erase(it);
+					send_to_char(ch, "Вы убрали spell %d.\r\n", spell);
+				}
 			}
-		} else {
-			if (IS_SET(value, SPELL_TEMP)) {
-//				send_to_char(ch, "Вы проверяете простановку темп .\r\n");
-				Temporary_Spells::add_spell(vict, spell, time(0), 60);
-			}
+		}
+		if (IS_SET(value, SPELL_TEMP)) {
+			send_to_char(ch, "Вы добавляете темп .\r\n");
+			Temporary_Spells::add_spell(vict, spell, time(0), 3600);
 		}
 		GET_SPELL_TYPE(vict, spell) = value;
 	} else if (SKILL_INVALID != skill
