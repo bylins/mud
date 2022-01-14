af2f22ba0b18884647441b7b5084c13b10131002
Svjatovit <sventovit@bk.ru>
2022-01-15 02:56:52 +0700

    The 'fighting round' otrigger was added for mobiles.

--- src/fightsystem/fight.cpp
+++ src/fightsystem/fight.cpp
@@ -1855,9 +1855,6 @@ void process_player_attack(CHAR_DATA *ch, int min_init) {
 		CLR_AF_BATTLE(ch, EAF_STAND);
 	}
 
-	// Срабатывание батл-триггеров амуниции
-	fight_otrigger(ch);
-
 	//* каст заклинания
 	if (ch->get_cast_spell() && GET_WAIT(ch) <= 0) {
 		if (AFF_FLAGGED(ch, EAffectFlag::AFF_SILENCE) || AFF_FLAGGED(ch, EAffectFlag::AFF_STRANGLED)) {
@@ -2087,24 +2084,22 @@ void perform_violence() {
 				|| !AWAKE(ch)) {
 				continue;
 			}
-
 			// If mob cast 'fear', 'teleport', 'recall', etc when initiative setted
-			if (!ch->get_fighting()
-				|| ch->in_room != IN_ROOM(ch->get_fighting())) {
+			if (!ch->get_fighting() || ch->in_room != IN_ROOM(ch->get_fighting())) {
 				continue;
 			}
-
-			if (initiative == 0) //везде в стоп-файтах ставится инициатива равная 0, убираем двойную атаку
-			{
+			//везде в стоп-файтах ставится инициатива равная 0, убираем двойную атаку
+			if (initiative == 0) {
 				continue;
 			}
-
 			//* выполнение атак в раунде
 			if (IS_NPC(ch)) {
 				process_npc_attack(ch);
 			} else {
 				process_player_attack(ch, min_init);
 			}
+			// Срабатывание батл-триггеров амуниции
+			fight_otrigger(ch);
 		}
 	}
 
