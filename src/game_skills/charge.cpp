f1a7850ddf349e0fcfd9ba5763b0e2f12ec1efc7
Svetodar <globus.hu@gmail.com>
2023-09-10 17:36:28 +0200

    Правки к "Натиску" - 2

--- src/game_skills/charge.cpp
+++ src/game_skills/charge.cpp
@@ -19,8 +19,16 @@
 void do_charge(CharData *ch, char *argument, int/* cmd*/, int/* subcmd*/) {
 	int direction;
 
+	if (!ch->GetSkill(ESkill::kCharge)) {
+		SendMsgToChar("Вы не знаете как.\r\n", ch);
+		return;
+	}
 	if (AFF_FLAGGED(ch, EAffect::kHold) || ch->get_wait() > 0) {
 		SendMsgToChar("Вы временно не в состоянии это сделать.\r\n", ch);
+		return;
+	}
+	if (ch->IsOnHorse()) {
+		SendMsgToChar("Верхом это сделать затруднительно.\r\n", ch);
 		return;
 	}
 	if (PRF_FLAGS(ch).get(EPrf::kIronWind)) {
@@ -77,48 +85,49 @@ void go_charge(CharData *ch, int direction) {
 	af2.location = EApply::kNone;
 	af2.battleflag = kAfSameTime;
 	af2.bitvector = to_underlying(EAffect::kConfused);
+	SetWait(ch, 2, false);
 
 	Damage dmg(SkillDmg(ESkill::kCharge), dam, fight::kPhysDmg, nullptr);
 	victims_amount = 2 + ((ch->GetSkill(ESkill::kCharge) / 40));
 
 	ActionTargeting::FoesRosterType roster{ch};
 	for (const auto target: roster) {
-		if (MOB_FLAGGED(target, EMobFlag::kProtect) || (!may_kill_here(ch, target, arg)) || target == ch || !CAN_SEE(ch, target)) {
+		if (MOB_FLAGGED(target, EMobFlag::kProtect) || (!may_kill_here(ch,target, arg)) ||target == ch || !CAN_SEE(ch,target)) {
 			--victims_amount;
 		} else {
-			if (AFF_FLAGGED(target, EAffect::kNoCharge) && (time(0) >= target->charge_apply_time + 4)) {
+			if (AFF_FLAGGED(target, EAffect::kNoCharge) && (time(0) >=target->charge_apply_time + 4)) {
 				act("$N0 уже на страже - вы не сможете повторно испугать $S своим натиском!",
-					false, ch, nullptr, target, kToChar);
+					false, ch, nullptr,target, kToChar);
 				hit(target, ch, ESkill::kUndefined, fight::kMainHand);
 			} else {
 				if (!AFF_FLAGGED(target, EAffect::kNoCharge)) {
 					affect_to_char(target, af);
 					target->charge_apply_time = time(0);
 				}
-				SkillRollResult result = MakeSkillTest(ch, ESkill::kCharge, target);
+				SkillRollResult result = MakeSkillTest(ch, ESkill::kCharge,target);
 				bool success = result.success;
 
-				TrainSkill(ch, ESkill::kCharge, success, target);
+				TrainSkill(ch, ESkill::kCharge, success,target);
 				TryToFindProtector(target, ch);
 				if (!success) {
 					act("Вам не удалось привести $N3 в замешательство своим бешенным натиском!",
-						false, ch, nullptr, target, kToChar);
+						false, ch, nullptr,target, kToChar);
 					act("$N попытал$U неожиданно напасть на Вас, но Вы вовремя спохватились и приняли бой!",
-						false, target, nullptr, ch, kToChar);
+						false,target, nullptr, ch, kToChar);
 					act("$n попытал$u обескуражить $N3 яростным натиском, но только рассмешил$g всех в округе.",
-						false, target, nullptr, ch, kToNotVict | kToArenaListen);
+						false,target, nullptr, ch, kToNotVict | kToArenaListen);
 					hit(target, ch, ESkill::kUndefined, fight::kMainHand);
 				} else {
 					if (ch->GetSkill(ESkill::kShieldBash) && (ch->GetSkill(ESkill::kBash)) && (GET_EQ(ch, kShield)) && (!PRF_FLAGGED(ch, EPrf::kAwake))) {
-						go_bash(ch, target);
+						go_bash(ch,target);
 					} else {
 						act("Хорошенько разогнавшись Вы прописали $N2 роскошного тумака!",
-							false, ch, nullptr, target, kToChar);
+							false, ch, nullptr,target, kToChar);
 						act("А вот вам и гостинец - $N одарил$G Вас смачной оплеухой!",
-							false, target, nullptr, ch, kToChar);
+							false,target, nullptr, ch, kToChar);
 						act("$N разогнал$U и отвесил$G $n2 внушительного тумака!",
-							false, target, nullptr, ch, kToNotVict | kToArenaListen);
-						dmg.Process(ch, target);
+							false,target, nullptr, ch, kToNotVict | kToArenaListen);
+						dmg.Process(ch,target);
 						affect_to_char(target, af2);
 					}
 				}
@@ -128,6 +137,5 @@ void go_charge(CharData *ch, int direction) {
 				break;
 			}
 		}
-		SetWait(ch, 2, true);
 	}
 }
