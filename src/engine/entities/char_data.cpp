e266f6b7e67aed19413456a27262134eb9d3d3f0
Bylins <stribog@bylins.su>
2025-01-15 21:43:38 +0300

    лишний лог

--- src/engine/entities/char_data.cpp
+++ src/engine/entities/char_data.cpp
@@ -649,7 +649,6 @@ void CharData::setSkillCooldown(ESkill skillID, unsigned cooldown) {
 unsigned CharData::getSkillCooldown(ESkill skillID) {
 	auto skillData = skills.find(skillID);
 	if (skillData != skills.end()) {
-		mudlog(fmt::format("запрос значения кулдауна {} равен {}", to_underlying(skillID), skillData->second.cooldown));
 		return skillData->second.cooldown;
 	}
 	return 0;
