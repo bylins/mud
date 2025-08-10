9110f79b23fd94f818e45b6d4182987954ff1c69
Prool <proolix@gmail.com>
2025-08-10 16:13:57 +0200

    prool: clans no expired

--- src/gameplay/clans/house.cpp
+++ src/gameplay/clans/house.cpp
@@ -85,7 +85,7 @@ int CLAN_CHEST_RNUM = -1;
 const int INGR_CHEST_VNUM = 333;
 int INGR_CHEST_RNUM = -1;
 // налог на хранилище ингров (в день)
-const int INGR_CHEST_TAX = 1000;
+const int INGR_CHEST_TAX = 1 /*1000*/; // prool
 // макс. длина сообщения дружины
 const int MAX_MOD_LENGTH = 3 * 80;
 // макс. длина названия ранга в дружине
@@ -485,7 +485,7 @@ void Clan::ClanLoadSingle(const std::string &index) {
 	tempClan->exp_history.load(tempClan->get_file_abbrev());
 	// иним на случай полной неактивности по итогам месяца, чтобы не было пропусков в списке
 	tempClan->exp_history.add_exp(0);
-	if (tempClan->exp_history.need_destroy() && !tempClan->test_clan) {
+	if (0/*tempClan->exp_history.need_destroy() && !tempClan->test_clan*/) { // prool: no expire clans
 		// клан-банк на воеводу
 		if (tempClan->bank > 0) {
 			Player t_victim;
@@ -3769,6 +3769,9 @@ int Clan::ChestTax() {
 	}
 	this->chest_objcount = count;
 	this->chest_discount = MAX(50, 75 - 5 * (this->clan_level));
+#if 1 // prool free chest tax
+return 0;
+#endif
 	return cost * this->chest_discount / 100;
 }
 
@@ -4391,6 +4394,9 @@ int Clan::ingr_chest_tax() {
 	}
 
 	ingr_chest_objcount_ = count;
+#if 1 // prool free chest tax
+return 0;
+#endif
 	return cost;
 }
 
@@ -4413,6 +4419,9 @@ void Clan::purge_ingr_chest() {
 }
 
 int Clan::calculate_clan_tax() const {
+#if 1 // prool free tax
+return 0;
+#endif
 	int cost = kClanTax + (storehouse * kClanStorehouseTax);
 
 	if (ingr_chest_active()) {
