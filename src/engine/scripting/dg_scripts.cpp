43b81ecdbe184860df111e6b0b16e68b533cc528
Bylins <stribog@bylins.su>
2026-01-09 18:15:20 +0300

    добавил проверку, вроде и не нужна но вдруг

--- src/engine/scripting/dg_scripts.cpp
+++ src/engine/scripting/dg_scripts.cpp
@@ -5674,7 +5674,9 @@ int timed_script_driver(void *go, Trigger *trig, int type, int mode) {
 		} else if (!strn_cmp("done", p, 4) || !strn_cmp("continue", p, 8)) {
 			if (*p == 'c') {
 				const auto temp = find_done(trig, cl);
-				cl->original = temp->original;
+				if (temp) {
+					cl->original = temp->original;
+				}
 			}
 			if (cl->original) {
 				auto orig_cmd = cl->original->cmd.c_str();
