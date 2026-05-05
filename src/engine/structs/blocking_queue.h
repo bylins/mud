f07b0b7cdf38e778cc714e752c70e2daa7ed8f62
Daniil-Gusev <i_guseva13@icloud.com>
2026-05-05 08:38:44 +0300

    Revert "fixed race condition in blocking queue"
    This reverts commit 7341429e7694710b59c3faed7c8e1dae54a55d4f.

--- src/engine/structs/blocking_queue.h
+++ src/engine/structs/blocking_queue.h
@@ -58,10 +58,7 @@ bool BlockingQueue<MessageType>::full() const {
 
 template<typename MessageType>
 void BlockingQueue<MessageType>::destroy() {
-	{
-		std::unique_lock<std::mutex> lock(m_messages_mutex);
-		m_destroying = true;
-	}
+	m_destroying = true;
 
 	m_new_message.notify_all();
 	m_message_processed.notify_all();
